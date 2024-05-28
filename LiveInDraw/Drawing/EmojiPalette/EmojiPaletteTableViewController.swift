f621f791baebdabc61e70f42bea0e513602f340f
aaakinc <88872703+yiyidaishui7@users.noreply.github.com>
2024-05-28 09:21:53 +0800

    Update EmojiPaletteTableViewController.swift

--- LiveInDraw/Drawing/EmojiPalette/EmojiPaletteTableViewController.swift
+++ LiveInDraw/Drawing/EmojiPalette/EmojiPaletteTableViewController.swift
@@ -7,8 +7,10 @@
 //  Copyright © 2024 AcroMace. All rights reserved.
 //
 
+// 符号管理器面板
 import UIKit
 
+// 选了表情
 protocol EmojiPaletteTableViewControllerDelegate: class {
     func didSelectEmoji(emoji: String)
 }
@@ -17,15 +19,20 @@ class EmojiPaletteTableViewController: UITableViewController {
 
     let emojiPalette = EmojiPalette()
     var delegate: EmojiPaletteTableViewControllerDelegate?
-
+    
+    // 添加新的表情符号
     @IBAction func didPressAddEmojiButton(_ sender: Any) {
+        // 提示用户输入新的表情符号
         let alert = UIAlertController(title: "Add emoji", message: nil, preferredStyle: .alert)
+        // okey 和 cancel
+        //okey：
         let okay = UIAlertAction(title: "OK", style: .default) { _ in
-            guard let alertTextField = alert.textFields?.first else { return }
+            guard let alertTextField = alert.textFields?.first else { return }//获取文本框里的符号
             guard let emoji = alertTextField.text else { return }
-            self.emojiPalette.addEmojiToPalette(emoji: emoji)
-            self.tableView.reloadData()
+            self.emojiPalette.addEmojiToPalette(emoji: emoji) //添加到emojiPalette中
+            self.tableView.reloadData() //刷新
         }
+        //cancel：不进行任何操作
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
 
         alert.addTextField { textField in
@@ -43,7 +50,7 @@ class EmojiPaletteTableViewController: UITableViewController {
 extension EmojiPaletteTableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return emojiPalette.count()
+        return emojiPalette.count() //
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
