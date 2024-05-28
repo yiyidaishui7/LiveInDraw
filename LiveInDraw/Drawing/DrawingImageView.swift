8480ca37d52c751ac4db6e28b7119b0c586640e2
aaakinc <88872703+yiyidaishui7@users.noreply.github.com>
2024-05-28 09:15:34 +0800

    Update DrawingImageView.swift

--- LiveInDraw/Drawing/DrawingImageView.swift
+++ LiveInDraw/Drawing/DrawingImageView.swift
@@ -9,6 +9,9 @@
 
 import UIKit
 
+//在用户触摸屏幕时检测触摸位置和力度
+
+//指定了 位置 大小
 protocol DrawingImageViewDelegate: class {
     func drewEmoji(at point: CGPoint, with size: CGFloat)
 }
@@ -16,20 +19,23 @@ protocol DrawingImageViewDelegate: class {
 class DrawingImageView: UIImageView {
     weak var delegate: DrawingImageViewDelegate?
 
+    //触摸开始
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         respondToTouches(touches)
     }
-
+    
+    //触摸移动
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesMoved(touches, with: event)
         respondToTouches(touches)
     }
 
+    //处理触摸
     private func respondToTouches(_ touches: Set<UITouch>) {
-        guard let touch = touches.first else { return }
-        let location = touch.location(in: self)
-        let size = touch.force > 0 ? touch.force : 1
-        delegate?.drewEmoji(at: location, with: size)
+        guard let touch = touches.first else { return }   //获取第一个触摸对象
+        let location = touch.location(in: self)           //触摸位置
+        let size = touch.force > 0 ? touch.force : 1      //触摸力度
+        delegate?.drewEmoji(at: location, with: size)     //传递位置、大小
     }
 }
